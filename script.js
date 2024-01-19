a2526a909cbb1941d50db38a53e639c59b351e60
Saroj Shrestha <sarojshrestha@Sarojs-iMac.local>
2024-01-19 15:42:20 -0700

    updated

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-/*
-An input for the user to type the answer. (Event Listener)
-An element displaying the current math equation question. (DOM Manipulation)
-Equations uses "+" operator only. (Function)
-A count down for the time left. (Interval, Time, DOM Manipulation)
-The game starts when the user clicks a button or starts typing in the input. (Event Listener)
-The game ends when the user runs out of time. (Interval, Time, Function)
-When the user makes a correct guess, add 1 second to the available time left. (Function)
-The current score is shown. (DOM Manipulation)
-A way to restart the game if time runs out. (Function)
-High score indicator for the high score in the current session. (Not covered in this walkthrough)
-*/
-
-
 $(document).ready(function(){
   var currentQuestion;
+  var interval;
+  var timeLeft = 10;
+  var score = 0;
+  
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  };
+  
+  var updateScore = function (amount) {
+    score += amount;
+    $('#score').text(score);
+  };
+  
+  var startGame = function () {
+    if (!interval) {
+      if (timeLeft === 0) {
+        updateTimeLeft(10);
+        updateScore(-score);
+      }
+      interval = setInterval(function () {
+        updateTimeLeft(-1);
+        if (timeLeft === 0) {
+          clearInterval(interval);
+          interval = undefined;
+        }
+      }, 1000);  
+    }
+  };
+  
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
-  }
+  };
   
   var questionGenerator = function () {
     var question = {};
@@ -27,26 +43,26 @@ $(document).ready(function(){
     question.equation = String(num1) + " + " + String(num2);
     
     return question;
-  }
+  };
   
-  currentQuestion = questionGenerator();
-  $('#equation').text(currentQuestion.equation);
-
   var renderNewQuestion = function () {
     currentQuestion = questionGenerator();
-    $('#equation').text(currentQuestion.equation);
-  }
-
+    $('#equation').text(currentQuestion.equation);  
+  };
+  
   var checkAnswer = function (userInput, answer) {
     if (userInput === answer) {
       renderNewQuestion();
       $('#user-input').val('');
+      updateTimeLeft(+1);
+      updateScore(+1);
     }
-  }
-
-  $('#user-input').on('keyup', function() {
+  };
+  
+  $('#user-input').on('keyup', function () {
+    startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
-
+  
   renderNewQuestion();
 });
